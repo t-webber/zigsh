357eb4c44b24a61d95ac47a61e4e79d2d5d49671
Tom Webber <t.c.w.webber@gmail.com>
2025-12-23 17:18:18 +0100

    feat(repl): add read/write I/O loop

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,26 @@
 const std = @import("std");
 
-pub fn main() !void {
-    std.debug.print("Hello, World!\n", .{});
+const File = std.fs.File;
+
+const alloc = std.alloc.page_allocator;
+
+const IoError = error{BufferOverflow} || File.WriteError || File.ReadError;
+
+pub fn main() IoError!void {
+    const out = File.stdout();
+    defer out.close();
+    const in = File.stdin();
+    defer in.close();
+
+    var len: usize = undefined;
+    var buf = [_]u8{0} ** 512;
+
+    while (true) {
+        try out.writeAll("$ ");
+        len = try in.read(&buf);
+        if (len == 512) return IoError.BufferOverflow;
+        _ = try out.write(">>> ");
+        _ = try out.write(buf[0 .. len - 1]);
+        try out.writeAll("\n");
+    }
 }
