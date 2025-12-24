5817859a3d9f82af2b79d0864bc16ddb3e7c2243
Tom Webber <t.c.w.webber@gmail.com>
2025-12-24 09:56:26 +0100

    feat(cwd): set/display cwd

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,9 @@
 const std = @import("std");
-
 const File = std.fs.File;
 
-const alloc = std.alloc.page_allocator;
+const Shell = @import("shell.zig").Shell;
 
-const IoError = error{BufferOverflow} || File.WriteError || File.ReadError;
+const IoError = error{ BufferOverflow, OutOfMemory } || File.WriteError || File.ReadError;
 
 pub fn main() IoError!void {
     const out = File.stdout();
@@ -12,15 +11,19 @@ pub fn main() IoError!void {
     const in = File.stdin();
     defer in.close();
 
+    var cwd: [512]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&cwd);
+    const allocator = fba.allocator();
+    var shell = try Shell.new(allocator);
+
     var len: usize = undefined;
-    var buf = [_]u8{0} ** 512;
+    var buf: [512]u8 = [_]u8{0} ** 512;
 
     while (true) {
-        try out.writeAll("$ ");
+        try out.writeAll(shell.get_ps1());
         len = try in.read(&buf);
         if (len == 512) return IoError.BufferOverflow;
-        _ = try out.write(">>> ");
-        _ = try out.write(buf[0 .. len - 1]);
-        try out.writeAll("\n");
+        if (shell.process_input(buf[0 .. len - 1])) |res|
+            try out.writeAll(res);
     }
 }
