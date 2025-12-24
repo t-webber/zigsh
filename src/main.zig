00163d2e8b95abd1b6608a8ab46fe5773be3c28a
Tom Webber <t.c.w.webber@gmail.com>
2025-12-24 17:36:30 +0100

    feat(env): use environment variables to store `PWD`

--- src/main.zig
+++ src/main.zig
@@ -11,19 +11,17 @@ pub fn main() IoError!void {
     const in = File.stdin();
     defer in.close();
 
-    var cwd: [512]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&cwd);
-    const allocator = fba.allocator();
-    var shell = try Shell.new(allocator);
+    var general_purpose_allocator: std.heap.GeneralPurposeAllocator(.{}) = .init;
+    const gpa = general_purpose_allocator.allocator();
+    var shell = try Shell.new(gpa);
 
-    var len: usize = undefined;
     var buf: [512]u8 = [_]u8{0} ** 512;
 
     while (true) {
         try out.writeAll(shell.get_ps1());
-        len = try in.read(&buf);
+        const len = try in.read(&buf);
         if (len == 512) return IoError.BufferOverflow;
-        if (shell.process_input(buf[0 .. len - 1])) |res|
+        if (try shell.process_input(buf[0 .. len - 1])) |res|
             try out.writeAll(res);
     }
 }
--- src/shell.zig
+++ src/shell.zig
@@ -1,36 +1,34 @@
 const std = @import("std");
 const mem = std.mem;
+const process = std.process;
 const Allocator = mem.Allocator;
-const File = std.fs.File;
 
 pub const Shell = struct {
-    cwd: struct { data: []u8, len: usize },
+    env: process.EnvMap,
 
-    pub fn new(allocator: Allocator) !@This() {
-        var buffer = try allocator.alloc(u8, 512);
-        buffer[0] = '/';
-        return @This(){ .cwd = .{ .data = buffer, .len = 1 } };
+    pub fn new(allocator: Allocator) process.GetEnvMapError!@This() {
+        return @This(){ .env = try process.getEnvMap(allocator) };
     }
 
     pub fn free(self: @This()) void {
-        self.cwd.free();
+        self.env.free();
     }
 
     pub fn get_ps1(self: *const @This()) []const u8 {
         var ps1 = [_]u8{0} ** (511 + 3);
-        @memcpy(ps1[0..self.cwd.len], self.cwd.data[0..self.cwd.len]);
-        @memcpy(ps1[self.cwd.len .. self.cwd.len + 3], " $ ");
-        return ps1[0 .. self.cwd.len + 3];
+        const pwd = self.env.get("PWD").?;
+        @memcpy(ps1[0..pwd.len], pwd);
+        @memcpy(ps1[pwd.len .. pwd.len + 3], " $ ");
+        return ps1[0 .. pwd.len + 3];
     }
 
-    pub fn process_input(self: *@This(), line: []const u8) ?[]const u8 {
+    pub fn process_input(self: *@This(), line: []const u8) !?[]const u8 {
         if (line.len < 3) return null;
         if (line[0] != 'p') return null;
         if (line[1] != ' ') return null;
         const dest = line[2..];
         for (dest) |ch| if (ch == ' ') return "Found more than 2 args for `p`.\n";
-        @memcpy(self.cwd.data[0..dest.len], dest);
-        self.cwd.len = dest.len;
+        try self.env.put("PWD", dest);
         return null;
     }
 };
